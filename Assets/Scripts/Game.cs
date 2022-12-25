9f7a6bf970f7b06a5ae0517e8012ec6ffd3d40cc
Nick Pateman <nick@npsoftware.co.uk>
2022-12-25 22:02:56 +0000

    Setup test arena with bunch of randomly placed objects and a single bot.

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,18 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Game : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject BotPrefab;
+    public GameObject ObstacleParent;
+    public GameObject BotsParent;
+    public Material ObstacleMaterial;
+    public int Width = 50;
+    public int Depth = 50;
+    public int ObstacleCount;
+
+    private GameObject _botGameObject;
+    private NavMeshSurface _navMeshSurface;
+
+
     void Start()
     {
-        
+        CreateObstacles();
+        GenerateNavMesh();
+        CreateBot();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void CreateBot()
+    {
+        var pos = new Vector3(
+                Random.Range(0, Width) - Width / 2,
+                0.5f,
+                Random.Range(0, Depth) - Depth / 2);
+        if (NavMesh.SamplePosition(
+            pos,
+            out var closestHit,
+            500f,
+            NavMesh.AllAreas))
+        {
+            _botGameObject = GameObject.Instantiate(
+                BotPrefab,
+                closestHit.position,
+                Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0)),
+                BotsParent.transform);
+        }
+    }
+
+    private void GenerateNavMesh()
+    {
+        var ground = transform.Find("Ground");
+        if(ground != null)
+        {
+            _navMeshSurface = ground.GetComponent<NavMeshSurface>();
+            _navMeshSurface.BuildNavMesh();
+        }
+    }
+
+    private void CreateObstacles()
+    {
+        for(var i = 0; i < ObstacleCount; i++)
+        {
+            var obstacle = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var pos = new Vector3(
+                Random.Range(0, Width) - Width / 2,
+                0.5f,
+                Random.Range(0, Depth) - Depth / 2);
+            var meshRenderer = obstacle.GetComponent<MeshRenderer>();
+            meshRenderer.sharedMaterial = ObstacleMaterial;
+            obstacle.transform.parent = ObstacleParent.transform;
+            obstacle.transform.position = pos;
+            meshRenderer.transform.rotation = Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0));
+        }
     }
 }
